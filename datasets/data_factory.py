4426942404124fba50f0dc74578c2779093e5f39
Henry <hps1945@live.co.uk>
2023-01-26 08:23:56 +0000

    train/test split

--- datasets/data_factory.py
+++ datasets/data_factory.py
@@ -1,23 +1,33 @@
-from datasets.flickr import Flickr8kDataset, MyCollate
-from torch.utils.data import DataLoader
-
+from typing import Tuple
+import torch
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
 
-def get_flickr8k(
+from datasets.flickr import Flickr8kDataset, MyCollate
+
+
+def get_flickr8k_data(
     root_folder,
     annotation_file,
     transform,
+    train_ratio=0.8,
     batch_size=32,
     num_workers=8,
     shuffle=True,
     pin_memory=True,
-):
+) -> Tuple[DataLoader, DataLoader, Dataset]:
     dataset = Flickr8kDataset(root_folder, annotation_file, transform=transform)
-
     pad_idx = dataset.vocab.stoi["<PAD>"]
+    train_size = int(train_ratio * len(dataset))
+    test_size = len(dataset) - train_size
+    
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, 
+                                                                [train_size, test_size], 
+                                                                 generator=torch.Generator().manual_seed(42))
 
-    loader = DataLoader(
-        dataset=dataset,
+
+    train_loader = DataLoader(
+        dataset=train_dataset,
         batch_size=batch_size,
         num_workers=num_workers,
         shuffle=shuffle,
@@ -25,20 +35,14 @@ def get_flickr8k(
         collate_fn=MyCollate(pad_idx=pad_idx),
     )
 
-    return loader, dataset
-
-
-
-
-if __name__ == "__main__":
-    transform = transforms.Compose(
-        [transforms.Resize((224, 224)), transforms.ToTensor(),]
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=False,
+        pin_memory=pin_memory,
+        collate_fn=MyCollate(pad_idx=pad_idx),
     )
 
-    loader, dataset = get_flickr8k(
-        "/homes/hps01/flickr8k/Flicker8k_Dataset/", "/homes/hps01/flickr8k/captions.txt", transform=transform
-    )
+    return train_loader, test_loader, dataset
 
-    for idx, (imgs, captions) in enumerate(loader):
-        print(imgs.shape)
-        print(captions.shape)
