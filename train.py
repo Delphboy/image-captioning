adfd731918710ad5533d93c0e6842f437d37ef56
Henry <hps1945@live.co.uk>
2023-06-12 13:38:21 +0100

    beam search

--- train.py
+++ train.py
@@ -54,6 +54,7 @@ def train_supervised(model: nn.Module,
             optimiser.step()
 
             epoch_loss.append(loss.item())
+        
         avg_epoch_loss = sum(epoch_loss) / len(epoch_loss)
         scheduler.step()
 
@@ -180,7 +181,7 @@ def evaluate(model: nn.Module,
             loss = F.cross_entropy(logits.permute(0, 2, 1), targets, ignore_index=data_loader.dataset.vocab.stoi["<PAD>"])
         
         losses.append(loss.item())
-        break
+    
     avg_loss = sum(losses)/len(losses)
     print(f"{split} loss: {avg_loss}")
     model.train()
